4e5537ce1233e7c45ea7385a98f63895e8126612
Krzysztof Kondrak <krzysztof.kondrak@gmail.com>
2020-06-13 21:33:51 +0200

    Properly close pictures.vga on palette read error.

--- parse_pictures.c
+++ parse_pictures.c
@@ -22,10 +22,11 @@ exit
  */
 
 unsigned char PALETTE[256][3];
-int picOffset = 0;
-int picCount = 0;
-int scale = 1;
-int alpha = 0;
+FILE *pictures_vga = NULL;
+int pictures_vga_offset = 0;
+int total_imgs = 0;
+int img_scale = 1;
+int rgba_export = 0;
 int yendor_version = 3;
 
 void loadPalette(const char *palName)
@@ -34,6 +35,7 @@ void loadPalette(const char *palName)
     if (!palFile)
     {
         printf("Could not open palette file: %s\n", palName);
+        fclose(pictures_vga);
         exit(1);
     }
 
@@ -50,25 +52,24 @@ void saveImage(int w, int h, int channels, unsigned char *buffer)
 
     for (int i = 0; i < w * h; ++i)
     {
-
         rgbData[index++] = PALETTE[buffer[i]][0];
         rgbData[index++] = PALETTE[buffer[i]][1];
         rgbData[index++] = PALETTE[buffer[i]][2];
         // treat magenta as transparency color
-        if (alpha)
+        if (rgba_export)
             rgbData[index++] = PALETTE[buffer[i]][0] == 0xFF && PALETTE[buffer[i]][1] == 0x00 && PALETTE[buffer[i]][2] == 0xFF ? 0x00 : 0xFF;
     }
 
     memset(filename, 0, sizeof(filename));
-    sprintf(filename, "pictures_y%d/%04d.png", yendor_version, picCount);
+    sprintf(filename, "pictures_y%d/%04d.png", yendor_version, total_imgs);
     printf("Extracting: %s\n", filename);
 
-    if (scale > 1)
+    if (img_scale > 1)
     {
-        unsigned char *rgbDataScaled = (unsigned char *)malloc(scale * scale * w * h * channels);
+        unsigned char *rgbDataScaled = (unsigned char *)malloc(img_scale * img_scale * w * h * channels);
 
-        stbir_resize_uint8(rgbData, w, h, 0, rgbDataScaled, scale * w, scale * h, 0, channels);
-        stbi_write_png(filename, scale * w, scale * h, channels, rgbDataScaled, scale * w * channels);
+        stbir_resize_uint8(rgbData, w, h, 0, rgbDataScaled, img_scale * w, img_scale * h, 0, channels);
+        stbi_write_png(filename, img_scale * w, img_scale * h, channels, rgbDataScaled, img_scale * w * channels);
 
         free(rgbDataScaled);
     }
@@ -78,15 +79,15 @@ void saveImage(int w, int h, int channels, unsigned char *buffer)
     }
 }
 
-void fetchImages(FILE *picturesVga, unsigned char *buffer, int w, int h, int c)
+void fetchImages(unsigned char *buffer, int w, int h, int c)
 {
-    int channels = alpha ? 4 : 3;
+    int channels = rgba_export ? 4 : 3;
     for (int i = 0; i < c; ++i)
     {
-        fseek(picturesVga, picOffset, 0);
-        int bytesRead = fread(buffer, sizeof(unsigned char), w * h, picturesVga);
+        fseek(pictures_vga, pictures_vga_offset, 0);
+        int bytesRead = fread(buffer, sizeof(unsigned char), w * h, pictures_vga);
         
-        if (feof(picturesVga))
+        if (feof(pictures_vga))
         {
             printf("Reached EOF.\n");
             break;
@@ -94,33 +95,33 @@ void fetchImages(FILE *picturesVga, unsigned char *buffer, int w, int h, int c)
 
         saveImage(w, h, channels, buffer);
 
-        picOffset += bytesRead;
-        picCount++;
+        pictures_vga_offset += bytesRead;
+        total_imgs++;
     }
 }
 
 int main(int argc, char **argv)
 {
-    unsigned char buffer[318 * 198];
-    char pictures_vga[32] = { "PICTURES.VGA" };
+    unsigned char buffer[318 * 198]; // maximum image size is 318x198
+    char picturesVga[32] = { "PICTURES.VGA" };
     
     for (int i = 1; i < argc; ++i)
     {        
         if (!strcmp(argv[i], "-a"))
         {
-            alpha = 1;
+            rgba_export = 1;
         }
 
         if (!strcmp(argv[i], "-s") && i < argc - 1)
         {
             if (isdigit(argv[i+1][0]))
-                scale = atoi(argv[i+1]);
+                img_scale = atoi(argv[i+1]);
         }
 
         if (!strcmp(argv[i], "-f") && i < argc - 1)
         {
-            memset(pictures_vga, 0, sizeof(pictures_vga));
-            strncpy(pictures_vga, argv[i+1], sizeof(pictures_vga));
+            memset(picturesVga, 0, sizeof(picturesVga));
+            strncpy(picturesVga, argv[i+1], sizeof(picturesVga));
         }
 
         if (!strcmp(argv[i], "-y2"))
@@ -145,10 +146,10 @@ int main(int argc, char **argv)
         }
     }
 
-    FILE *picturesVga = fopen(pictures_vga, "rb");
+    pictures_vga = fopen(picturesVga, "rb");
     if (!picturesVga)
     {
-        printf("Could not load %s!\n", pictures_vga);
+        printf("Could not load %s!\n", picturesVga);
         return 1;
     }
 
@@ -158,62 +159,62 @@ int main(int argc, char **argv)
 
         // fullscreen backgrounds
         loadPalette("palettes/sw_logo.pal");
-        fetchImages(picturesVga, buffer, 318, 198, 1);
+        fetchImages(buffer, 318, 198, 1);
         loadPalette("palettes/primary.pal");
-        fetchImages(picturesVga, buffer, 318, 198, 4);
+        fetchImages(buffer, 318, 198, 4);
         loadPalette("palettes/y2_intro.pal");
-        fetchImages(picturesVga, buffer, 318, 198, 2);
+        fetchImages(buffer, 318, 198, 2);
         loadPalette("palettes/primary.pal");
-        fetchImages(picturesVga, buffer, 318, 198, 4);
+        fetchImages(buffer, 318, 198, 4);
         loadPalette("palettes/y2_thaine.pal");
-        fetchImages(picturesVga, buffer, 318, 198, 1);
+        fetchImages(buffer, 318, 198, 1);
         loadPalette("palettes/y2_cluebook.pal");
-        fetchImages(picturesVga, buffer, 318, 198, 1);
+        fetchImages(buffer, 318, 198, 1);
         loadPalette("palettes/primary.pal");
-        fetchImages(picturesVga, buffer, 318, 198, 2);
+        fetchImages(buffer, 318, 198, 2);
         loadPalette("palettes/order.pal");
-        fetchImages(picturesVga, buffer, 318, 198, 1);
+        fetchImages(buffer, 318, 198, 1);
         loadPalette("palettes/webfoot.pal");
-        fetchImages(picturesVga, buffer, 318, 198, 1);
+        fetchImages(buffer, 318, 198, 1);
         loadPalette("palettes/primary.pal");
-        fetchImages(picturesVga, buffer, 318, 198, 3);
+        fetchImages(buffer, 318, 198, 3);
         PALETTE[255][1] = 0xFF;
-        fetchImages(picturesVga, buffer, 318, 198, 1);
+        fetchImages(buffer, 318, 198, 1);
         PALETTE[255][1] = 0x00;
-        fetchImages(picturesVga, buffer, 318, 198, 2);
+        fetchImages(buffer, 318, 198, 2);
         // scene objects and walls
-        fetchImages(picturesVga, buffer, 210, 105, 73);
+        fetchImages(buffer, 210, 105, 73);
         loadPalette("palettes/y2_intro.pal");
-        fetchImages(picturesVga, buffer, 210, 105, 6);
+        fetchImages(buffer, 210, 105, 6);
         loadPalette("palettes/primary.pal");
-        fetchImages(picturesVga, buffer, 210, 105, 22);
+        fetchImages(buffer, 210, 105, 22);
         // narrow objects and enemies
-        fetchImages(picturesVga, buffer, 140, 155, 10);
+        fetchImages(buffer, 140, 155, 10);
         loadPalette("palettes/y2_intro.pal");
-        fetchImages(picturesVga, buffer, 140, 155, 6);
+        fetchImages(buffer, 140, 155, 6);
         loadPalette("palettes/primary.pal");
-        fetchImages(picturesVga, buffer, 140, 155, 199);
+        fetchImages(buffer, 140, 155, 199);
         // lower + wider objects and enemies
-        fetchImages(picturesVga, buffer, 190, 110, 162);
+        fetchImages(buffer, 190, 110, 162);
         // floors and ceilings
-        fetchImages(picturesVga, buffer, 224, 74, 18);
+        fetchImages(buffer, 224, 74, 18);
         // lower floors and ceilings
         PALETTE[255][1] = 0xFF;
-        fetchImages(picturesVga, buffer, 224, 62, 2);
+        fetchImages(buffer, 224, 62, 2);
         PALETTE[255][1] = 0x00;
-        fetchImages(picturesVga, buffer, 224, 62, 10);
+        fetchImages(buffer, 224, 62, 10);
         // paper dolls and paper doll effects
-        fetchImages(picturesVga, buffer, 56, 136, 23);
+        fetchImages(buffer, 56, 136, 23);
         loadPalette("palettes/y2_intro.pal");
-        fetchImages(picturesVga, buffer, 56, 136, 3);
+        fetchImages(buffer, 56, 136, 3);
         loadPalette("palettes/primary.pal");
-        fetchImages(picturesVga, buffer, 56, 136, 29);
+        fetchImages(buffer, 56, 136, 29);
         // portraits and portrait effects
-        fetchImages(picturesVga, buffer, 32, 32, 270);
+        fetchImages(buffer, 32, 32, 270);
         // icons
-        fetchImages(picturesVga, buffer, 16, 16, 510);
+        fetchImages(buffer, 16, 16, 510);
         // smaller icons
-        fetchImages(picturesVga, buffer, 8, 8, 116);
+        fetchImages(buffer, 8, 8, 116);
     }
 
     if (yendor_version == 3)
@@ -222,48 +223,48 @@ int main(int argc, char **argv)
 
         // fullscreen backgrounds
         loadPalette("palettes/sw_logo.pal");
-        fetchImages(picturesVga, buffer, 318, 198, 1);
+        fetchImages(buffer, 318, 198, 1);
         loadPalette("palettes/primary.pal");
-        fetchImages(picturesVga, buffer, 318, 198, 4);
+        fetchImages(buffer, 318, 198, 4);
         loadPalette("palettes/y3_cluebook.pal");
-        fetchImages(picturesVga, buffer, 318, 198, 1);
+        fetchImages(buffer, 318, 198, 1);
         loadPalette("palettes/y3_intro.pal");
-        fetchImages(picturesVga, buffer, 318, 198, 6);
+        fetchImages(buffer, 318, 198, 6);
         loadPalette("palettes/y3_title.pal");
-        fetchImages(picturesVga, buffer, 318, 198, 1);
+        fetchImages(buffer, 318, 198, 1);
         loadPalette("palettes/primary.pal");
-        fetchImages(picturesVga, buffer, 318, 198, 2);
+        fetchImages(buffer, 318, 198, 2);
         loadPalette("palettes/order.pal");
-        fetchImages(picturesVga, buffer, 318, 198, 1);
+        fetchImages(buffer, 318, 198, 1);
         loadPalette("palettes/webfoot.pal");
-        fetchImages(picturesVga, buffer, 318, 198, 1);
+        fetchImages(buffer, 318, 198, 1);
         loadPalette("palettes/primary.pal");
-        fetchImages(picturesVga, buffer, 318, 198, 6);
+        fetchImages(buffer, 318, 198, 6);
         // scene objects and walls
-        fetchImages(picturesVga, buffer, 210, 105, 34);
+        fetchImages(buffer, 210, 105, 34);
         loadPalette("palettes/sw_logo.pal");
-        fetchImages(picturesVga, buffer, 210, 105, 12);
+        fetchImages(buffer, 210, 105, 12);
         loadPalette("palettes/primary.pal");
-        fetchImages(picturesVga, buffer, 210, 105, 110);
+        fetchImages(buffer, 210, 105, 110);
         // narrow objects and enemies
-        fetchImages(picturesVga, buffer, 140, 155, 270);
+        fetchImages(buffer, 140, 155, 270);
         // lower + wider objects and enemies
-        fetchImages(picturesVga, buffer, 190, 110, 238);
+        fetchImages(buffer, 190, 110, 238);
         // floors and ceilings
-        fetchImages(picturesVga, buffer, 224, 74, 28);
+        fetchImages(buffer, 224, 74, 28);
         // lower floors and ceilings
-        fetchImages(picturesVga, buffer, 224, 62, 14);
+        fetchImages(buffer, 224, 62, 14);
         // paper dolls and paper doll effects
-        fetchImages(picturesVga, buffer, 56, 136, 70);
+        fetchImages(buffer, 56, 136, 70);
         // portraits and portrait effects
-        fetchImages(picturesVga, buffer, 32, 32, 180);
+        fetchImages(buffer, 32, 32, 180);
         // icons
-        fetchImages(picturesVga, buffer, 16, 16, 340);
+        fetchImages(buffer, 16, 16, 340);
         // smaller icons
-        fetchImages(picturesVga, buffer, 8, 8, 145);
+        fetchImages(buffer, 8, 8, 145);
     }
 
-    fclose(picturesVga);
+    fclose(pictures_vga);
 
     return 0;
 }
